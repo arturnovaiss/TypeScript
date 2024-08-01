edff330562a592a8de7da68e3aa01654101e29be
arturnovaiss <arturcomercialpro@gmail.com>
2024-08-01 18:48:55 -0400

    first commit

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+// Exemplo de uso:
+var resultadoMultiplicacao = multiplicar(3, 4);
+console.log(resultadoMultiplicacao); // Output: 12
+function saudacao(nome) {
+    return "Ol\u00E1 ".concat(nome);
+}
+// Exemplo de uso:
+var mensagemSaudacao = saudacao("João");
+console.log(mensagemSaudacao); // Output: Olá João
---
+++ funcoes.ts
@@ -0,0 +1,15 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Exemplo de uso:
+const resultadoMultiplicacao = multiplicar(3, 4);
+console.log(resultadoMultiplicacao); // Output: 12
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+// Exemplo de uso:
+const mensagemSaudacao = saudacao("João");
+console.log(mensagemSaudacao); // Output: Olá João
